a6aeeb04c6781f1b831ac9ace93b227641d2179f
Calvin Thang <calvin@Calvins-MacBook-Pro.local>
2018-08-24 17:17:25 -0400

    Commit message

---
+++ README.md
@@ -0,0 +1,2 @@
+This program calculates percentages based on the bill amount.
+# tipCalculator
---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Calvin Thang on 8/23/18.
+//  Copyright © 2018 Calvin Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tipPercentages = [0.18, 0.20, 0.25]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+}
+
